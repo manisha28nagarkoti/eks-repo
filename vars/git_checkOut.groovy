4e223290884c87b992b881d1e3308adf697f6c49
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-05-01 22:43:26 +0530

    Create git_checkOut.groovy

---
+++ vars/git_checkOut.groovy
@@ -0,0 +1,11 @@
+def call(string repo_url){ 
+withCredentials([sshUserPrivateKey(credentialsId: env.credential_git, keyFileVariable: 'SSH_KEY')]) {
+                    sh """
+                        eval `ssh-agent`
+                        ssh-add $SSH_KEY
+                        ssh-keyscan github.com >> ~/.ssh/known_hosts
+                        git clone $repo_url
+                    """
+                }
+  
+}
