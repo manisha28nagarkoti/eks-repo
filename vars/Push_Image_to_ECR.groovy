9c7b62bd314ae90bbb0cdecc168bd9f6f03452c6
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-05-02 16:43:14 +0530

    Create Push_Image_to_ECR.groovy

---
+++ vars/Push_Image_to_ECR.groovy
@@ -0,0 +1,9 @@
+def call(String ecr_repo,String repo_name,String tag,String image_name,String tag){
+sh '''
+docker tag $image_name:$tag $ecr_repo/$repo_name:$tag
+docker push $ecr_repo/$repo_name:$tag
+
+'''
+
+
+}
