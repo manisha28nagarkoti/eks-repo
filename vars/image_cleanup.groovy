554ec5e3446df7acad9419bfd0df59f775f36ad9
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-05-03 11:07:43 +0530

    Update image_cleanup.groovy

--- vars/image_cleanup.groovy
+++ vars/image_cleanup.groovy
@@ -1,5 +1,5 @@
-def call(String image_name,String tag){
-  
+def call(String ecr_repo,String repo_name,String tag){
+  def image_name = $ecr_repo/$repo_name
 sh 'echo removing $image_name and $image_name:$tag'
   sh 'docker rmi $image_name $image_name:$tag'
 
